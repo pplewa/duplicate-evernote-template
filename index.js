6181d42d777deadbec84b9a1070d8d3c92d8d096
Piotr Plewa <pplewa@atlassian.com>
2016-07-29 10:38:12 +1000

    add babel

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-const Evernote = require('evernote').Evernote
-const promisify = require('es6-promisify')
-
-const evernoteClient = new Evernote.Client({ token: process.env.EVERNOTE_TOKEN, sandbox: false })
-const noteStore = evernoteClient.getNoteStore()
-const copyNote = promisify(noteStore.copyNote)
-const [,,noteGuid, toNotebookGuid] = process.argv
-
-if (!noteGuid || !toNotebookGuid) {
-	throw new Error('missing noteGuid or toNotebookGuid arguments')
-}
-
-copyNote(noteGuid, toNotebookGuid)
-	.then(() => console.log('OK'))
-	.catch((error) => { throw new Error(error) })
+require('babel-register')
+module.exports = require('./copy-template')
