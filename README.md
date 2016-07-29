54acf4980cb00671f88fc011ef8fda89d1f48b0b
Piotr Plewa <pplewa@atlassian.com>
2016-07-29 13:34:27 +1000

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Duplicate Evernote Note Template
+
+Duplicates Evernote for given noteGuid and destination notebookGuid
+
+## Usage
+
+```
+const copyNote = require('duplicate-evernote-template')
+
+copyNote(evernoteToken, noteGuid, toNotebookGuid)
+
+```
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const Evernote = require('evernote').Evernote
 const promisify = require('es6-promisify')
 
-const evernoteClient = new Evernote.Client({ token: process.env.EVERNOTE_TOKEN, sandbox: false })
-const noteStore = evernoteClient.getNoteStore()
-const copyNote = promisify(noteStore.copyNote)
-const noteGuid = process.argv[2]
-const toNotebookGuid = process.argv[3]
+function copyTemplate(evernoteToken, noteGuid, toNotebookGuid) {
+	console.log(evernoteToken, noteGuid, toNotebookGuid)
+	if (!evernoteToken || !noteGuid || !toNotebookGuid) {
+		throw new Error('missing one of the arguments')
+	}
 
-if (!noteGuid || !toNotebookGuid) {
-	throw new Error('missing noteGuid or toNotebookGuid arguments')
+	const evernoteClient = new Evernote.Client({ token: evernoteToken, sandbox: false })
+	const noteStore = evernoteClient.getNoteStore()
+	const copyNote = promisify(noteStore.copyNote)
+
+	return copyNote(noteGuid, toNotebookGuid)
 }
 
-copyNote(noteGuid, toNotebookGuid)
-	.then(() => console.log('OK'))
-	.catch((error) => { throw new Error(error) })
+module.exports = copyTemplate
